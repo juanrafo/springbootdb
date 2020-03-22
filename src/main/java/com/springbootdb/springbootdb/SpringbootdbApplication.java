c81e481206379c0cd02e9aec0a505fcc0f424d26
juanrafo <sagitario2112@gmail.com>
2020-03-22 15:24:20 -0500

    comit config springboot

--- src/main/java/com/springbootdb/springbootdb/SpringbootdbApplication.java
+++ src/main/java/com/springbootdb/springbootdb/SpringbootdbApplication.java
@@ -1,9 +1,15 @@
 package com.springbootdb.springbootdb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({ "com.springbootdb.springbootdb", "controller" })
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringbootdbApplication {
 
